ecdd1d68391d69a9fd1814ef2eba3175a3ad82fd
Marco Antônio Gusmão Carvalho <marcocarvalho.web@hotmail.com>
2020-09-30 08:28:33 -0300

    Initial commit

---
+++ .env/.config.inc.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+$i = 0;
+$i++;
+$cfg['Servers'][$i]['auth_type'] = 'config';
+$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['compress'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
+$cfg['Servers'][$i]['user'] = 'root';
+$cfg['Servers'][$i]['password'] = ''; 
